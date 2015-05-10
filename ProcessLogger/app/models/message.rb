357f2260b3630d01c1f88b07d488756637665f25
Blake Pritchard <blakepritchared@yahoo.com>
2015-05-10 18:33:09 -0500

    blakecommit

--- ProcessLogger/app/models/message.rb
+++ ProcessLogger/app/models/message.rb
@@ -1,8 +1,13 @@
+require 'twilio-ruby'
+
 class Message < ActiveRecord::Base
   validates :process, :presence => true
   validates :sender, :presence => true
 
-
+  before_save do
+    x = JSON.parse(self.text)
+    self.adc1 = x['ADC1']
+  end
 
 
 end
