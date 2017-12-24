d49a248335410e130cb0d51f39e05285f520bb74
Blake Pritchard <BlakePritchard@yahoo.com>
2017-12-24 08:35:32 -0600

    adding Git Repo for Adafruit Python 9685 driver to Setup file

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/setup.sh
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/setup.sh
@@ -2,3 +2,6 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:gpredict-team/ppa
 sudo apt-get update
+
+cd ~/src/git/IndustrialAutomation/RadioAntenna/PiCode/
+git clone https://github.com/adafruit/Adafruit_Python_PCA9685.git
