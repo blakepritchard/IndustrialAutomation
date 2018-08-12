924c82cfd1d8742921e456a7c4895068bc92c8e5
Blake Pritchard <BlakePritchard@yahoo.com>
2018-08-12 16:24:46 -0500

    time

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/Rotator/Rotator.py
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/Rotator/Rotator.py
@@ -148,7 +148,7 @@ class Rotator(object):
                     self._isOrientationRunning = self._orientation.begin()  #Start BNO055 Orientation Sensor
                     
                 except RuntimeError as error:
-                    print("BNO055 Chip Not Initialized. Will Attempt in" + str(nSleepTime) +" seconds. Attemps Left:" +str(nRetry))
+                    print("BNO055 Chip Not Initialized. Will Attempt in: " + str(nSleepTime) +" seconds. Attemps Left:" +str(nRetry))
                     print(type(error))    # the exception instance
                     print(error.args)     # arguments stored in .args
                     time.sleep(nSleepTime)
