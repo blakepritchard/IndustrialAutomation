1dc2dcebb0ce878e63a6902df63c41586f5671e2
Blake Pritchard <BlakePritchard@yahoo.com>
2018-11-29 17:54:18 -0600

    handle serial exception

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/SatTrackerPiDaemon.py
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/SatTrackerPiDaemon.py
@@ -160,8 +160,10 @@ USAGE
         return 0
     
     except serial.SerialException as e:
-        print(e.msg)
-        raise       # XXX handle instead of re-raise?
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
 
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
