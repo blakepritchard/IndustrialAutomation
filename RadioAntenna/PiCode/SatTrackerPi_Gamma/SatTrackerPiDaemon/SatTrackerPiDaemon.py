b72a2a8c3820a334d8eb1e36094403384c726dfe
Blake Pritchard <BlakePritchard@yahoo.com>
2019-01-01 19:50:44 -0600

    still trying to fix verbosity

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/SatTrackerPiDaemon.py
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/SatTrackerPiDaemon.py
@@ -119,7 +119,11 @@ USAGE
                 print("Verbose mode on Log Level: "+str(verbose))       
         if(''==name_port_rotctl): 
             name_port_rotctl = '/dev/ttyUSB1'
+        if(''==name_port_website): 
+            name_port_rotctl = '/dev/ttyUSB2'    
+
         print("RotCtl Port: " + name_port_rotctl)
+        print("WebSite Port: " + name_port_website)
 
         """if(''==serial_port_speed): 
                 serial_port_speed = 9600
@@ -128,7 +132,6 @@ USAGE
         print("Opening serial port for rotctl.")
         serial_port_rotctl = serial.Serial(name_port_rotctl, 9600, rtscts=True,dsrdtr=True, timeout=0)
 
-        print("WebSite Port: " + name_port_website)
         print("Opening serial port for website.")
         serial_port_website = serial.Serial(name_port_website, 9600, rtscts=True,dsrdtr=True, timeout=0)
 
@@ -147,7 +150,8 @@ USAGE
             byte_next_rotctl = serial_port_rotctl.read()
             char_next_rotctl = byte_next_rotctl.decode("utf-8")
 
-            if verbose > 4: print('-')
+            if verbose > 4: 
+                print('-')
             if byte_next_rotctl:
                 
                 if ((byte_next_rotctl == bytes_carraigereturn) or (byte_next_rotctl == bytes_linefeed)):
@@ -160,7 +164,8 @@ USAGE
                     print_newline = True 
                 else:
                     command_rotctl += char_next_rotctl
-                    if verbose > 3: print(char_next_rotctl)
+                    if verbose > 3: 
+                        print(char_next_rotctl)
                     if print_newline:
                         print','
                         print_newline = False
@@ -172,11 +177,13 @@ USAGE
             byte_next_website = serial_port_website.read()
             char_next_website = byte_next_website.decode("utf-8")
 
-            if verbose > 4: print('-')
+            if verbose > 4: 
+                print('-')
             if byte_next_website:
                 
                 if ((byte_next_website == bytes_carraigereturn) or (byte_next_website == bytes_linefeed)):
-                    if verbose > 2: print(command_website)
+                    if verbose > 2: 
+                        print(command_website)
                     website_response = device_rotator.execute_website_command(command_website)
                     serial_port_website.write(website_response)
                     command_website = ""  
@@ -185,7 +192,8 @@ USAGE
                     print_newline = True 
                 else:
                     command_website += char_next_website
-                    if verbose > 3: print(char_next_website)
+                    if verbose > 3: 
+                        print(char_next_website)
                     if print_newline:
                         print','
                         print_newline = False
@@ -193,7 +201,8 @@ USAGE
                 char_next_website = ''
                 byte_next_website = 0
         
-            if verbose > 5: print('.')
+            if verbose > 5: 
+                print('.')
 
     except KeyboardInterrupt:
         ### handle keyboard interrupt ###
