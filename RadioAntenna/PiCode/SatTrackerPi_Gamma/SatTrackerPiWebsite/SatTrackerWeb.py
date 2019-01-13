9e17ce5756f9d6a5b0da65c19423b77e8949392b
Blake Pritchard <BlakePritchard@yahoo.com>
2019-01-12 22:23:13 -0600

    more

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebsite/SatTrackerWeb.py
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebsite/SatTrackerWeb.py
@@ -46,7 +46,7 @@ def set_polarity():
 def send_serial_command(serial_command):
     try:
         serial_port_name = sat_tracker_app.config['SERIAL_PORT_NAME']
-        print("About to Send Serial Command to: "+ str(serial_port_name) )
+        print("User: " + os.environ.get('USER')+ " is about to Send Serial Command to: "+ str(serial_port_name) )
         serial_port_website = serial.Serial(str(serial_port_name), 9600, rtscts=True,dsrdtr=True, timeout=0) 
         serial_port_website.write(serial_command.encode())
         return redirect("http://"+socket.gethostname()+"/polarity", code=302)
