11f14a68a7a4fa0d02c9410ab429e51b26c19025
Blake Pritchard <BlakePritchard@yahoo.com>
2019-02-25 20:36:21 -0600

    changing ownership of Website directory and database

---
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebsite/scripts/database_setup.py
@@ -0,0 +1,20 @@
+#python
+
+from SatTrackerWeb import db
+from SatTrackerWeb import Rotator
+from SatTrackerWeb import RotatorCommand
+db.create_all()
+rotator = Rotator()
+rotator.rotator_name="SatTrackerPi"
+rotator.azimuth_steps=0
+rotator.azimuth_degrees=0
+rotator.elevation_steps = 0
+rotator.elevation_degrees=0
+rotator.polarity_steps=0
+rotator.polarity_degrees=0
+rotator.polarity_is_tracking=False
+rotator.polarity_tracking_speed=0
+db.session.add(rotator)
+db.session.commit()
+db.session.close()
+#exit()
