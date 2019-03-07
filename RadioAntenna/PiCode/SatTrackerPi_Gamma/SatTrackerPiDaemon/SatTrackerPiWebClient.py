758c7b335cd54491fda7bfe3c959f91f75c1e850
Blake Pritchard <BlakePritchard@yahoo.com>
2019-03-06 20:55:07 -0600

    building web client

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/SatTrackerPiWebClient.py
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/SatTrackerPiWebClient.py
@@ -1,168 +1,202 @@
+import os
 import sys
+import pwd
+
 import serial
 import logging
+import json
+import requests 
 import argparse
+
+import sched
+import time
 
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 
 def main(argv=None):
-    parser = optparse.OptionParser()
-
     if argv is None:
         argv = sys.argv
     else:
         sys.argv.extend(argv)
 
-    parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+    parser = ArgumentParser(description="SatTrackerPi Web Client", formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument("-l", "--loglevel", dest="loglevel", help="set loglevel level [default: %(default)s]")
-    parser.add_argument("-r", "--rotctl", dest="rotctl", help="set rotctl-gpredict serial port [default: %(default)s]")
+    parser.add_argument("-r", "--rotator", dest="rotator", help="set tracker serial port name for output[default: %(default)s]")
     parser.add_argument("-s", "--speed", dest="speed", type=int, help="set serial port speed [default: %(default)s]")
     parser.add_argument("-w", "--webserver", dest="webserver", help="set SatTrackerWebsite webserver URL [default: %(default)s]")
+    parser.add_argument("-i", "--interval", dest="interval", help="set Interval between Status Updates")
     serial_config_filename = ("/home/pi/src/git/IndustrialAutomation/RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiDaemon/webclient_serial.config")
 
     # Process arguments
     args = parser.parse_args()
-    verbose = args.loglevel
-    name_port_rotctl = args.rotctl
-    url_webserver = args.webserver
-    speed_serial = args.speed
-
     logging.basicConfig(filename='sat_tracker_webclient.log', filemode='w', level=int(args.loglevel), format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     logging.info("Verbose mode on Log Level: "+str(args.loglevel))
 
-
+    sat_tracker_webclient = SatTrackerPiWebClient(args.loglevel, args.rotator, args.speed, args.webserver, args.interval)
+    sat_tracker_webclient.start_client_loop()
 
 if __name__ == "__main__":
     sys.exit(main())
 
 
 
+class SatTrackerPiWebClient:
 
-def get_rotator_status():
-    azimuth_current = execute_serial_command("AZ", None)
-    elevation_current = execute_serial_command("EL", None)
-    polarity_current = execute_serial_command("PO", None)
-
-#c@sat_tracker_app.route("/sat_tracker/api/rotator/status", methods=["GET"])
-def get_rotator_status():
-    try:
-        json_result = execute_serial_command("RS", None)
-        logging.debug("Rotator Status: " + json_result)
-        return json_result
+    def __init__(self,  verbose, name_port_rotator, speed_serial, url_webserver, interval):
+        self.verbose = verbose
+        self.name_port_rotator = name_port_rotator
+        self.url_webserver = url_webserver
+        self.speed_serial = speed_serial
+        self.interval = interval
+        self.start_time = time.time()
+        self.scheduler = sched.scheduler(time.time, time.sleep)
     
-    except Exception as exception:
-        return handle_web_exception(exception)
+    def start_client_loop(self):
+        try:
+            self.scheduler.enter(self.interval, 1, self._execute_client_loop(), ())
+            self.scheduler.run()
+        except Exception as exception:
+            return self.handle_exception(exception)
+
+    def _execute_client_loop(self):
+        try:
+            self.start_time = time.time()
+            self.post_rotator_status()
+            current_time = time.time()
+            interval_next = self.interval- (( current_time - self.start_time) % self.interval )
+            self.scheduler.enter(interval_next, 1, self._execute_client_loop(), ())
+        except Exception as exception:
+            return self.handle_exception(exception)
+
+    def post_rotator_status(self):
+        try:
+            data = self.get_rotator_status()
+            r = requests.post(url = self.url_webserver + "/sat_tracker/api/rotator/status", data = data)        
+            return r.text
+        except Exception as exception:
+            return self.handle_exception(exception)       
 
 
+    def get_rotator_status(self):
+        try:
+            json_result = self.execute_serial_command("RS", None)
+            logging.debug("Rotator Status: " + json_result)
+            return json_result
+        
+        except Exception as exception:
+            return self.handle_exception(exception)
 
-#@sat_tracker_app.route("/sat_tracker/api/rotator/polarity", methods=["POST"])
-def set_polarity_json():
-    try:
-        json_result = ""
-        sat_tracker_app.logger.info("a POST to set_polarity_json Has Been Recieved with data: " + request.get_data())
-        obj_polarity_next = request.get_json(force=True)
-        float_polarity_next = obj_polarity_next['polarity_new']
-        if request.method == 'POST':
+
+    def set_polarity_json(self, polarity_json):
+        try:
+            json_result = ""
+            logging.info("a set_polarity_json command is being sent with data: " + polarity_json)
+
+            float_polarity_next = polarity_json['polarity_new']
+ 
             polarity_command= "PP" +  str(float_polarity_next)
-            polarity_current = execute_serial_command(polarity_command, None)
+            polarity_current = self.execute_serial_command(polarity_command, None)
             json_result = {"polarity_degrees": polarity_current}
-        return json.dumps(json_result)
+            return json.dumps(json_result)
 
-    except Exception as exception:
-        return handle_web_exception(exception)
-
-
-#@sat_tracker_app.route("/sat_tracker/set_elevation", methods=["GET","POST"])
-#@sat_tracker_app.route("/elevation/set_elevation", methods=["POST"])
-def set_elevation():
-    try:
-        sat_tracker_app.logger.debug("a POST to set_elevation Has Been Recieved")
-        if request.method == 'POST':
-            polarity_command= "EL" + request.form['elevation_new'] 
-            execute_serial_command(polarity_command)
-        return redirect("http://"+socket.gethostname()+"/sat_tracker/", code=302)
-
-    except Exception as exception:
-        return handle_web_exception(exception)
+        except Exception as exception:
+            return self.handle_exception(exception)
 
 
-#@sat_tracker_app.route("/sat_tracker/set_azimuth", methods=["GET","POST"])
-#@sat_tracker_app.route("/azimuth/set_azimuth", methods=["POST"])
-def set_azimuth():
-    try:
-        sat_tracker_app.logger.debug("a POST to set_azimuth Has Been Recieved")
-        if request.method == 'POST':
-            polarity_command= "AZ" + request.form['azimuth_new'] 
-            execute_serial_command(polarity_command)
-        return redirect("http://"+socket.gethostname()+"/sat_tracker/", code=302)
 
-    except Exception as exception:
-        return handle_web_exception(exception)
+    def set_elevation(self, elevation_new):
+        try:
+            logging.debug("a set_elevation Command Is Being Sent")
+
+            polarity_command= "EL" + elevation_new 
+            result = self.execute_serial_command(polarity_command)
+            return result
+
+        except Exception as exception:
+            return self.handle_exception(exception)
 
 
+    #@sat_tracker_app.route("/sat_tracker/set_azimuth", methods=["GET","POST"])
+    #@sat_tracker_app.route("/azimuth/set_azimuth", methods=["POST"])
+    def set_azimuth(self, azimuth_new):
+        try:
+            logging.debug("a set_azimuth Command Is Being Sent")
+
+            polarity_command= "AZ" + azimuth_new 
+            result = self.execute_serial_command(polarity_command)
+
+            return result
+
+        except Exception as exception:
+            return self.handle_exception(exception)
 
 
 
 
-# Send Serial Command, Get Serial Response
-def execute_serial_command(serial_command, serial_timeout=0):
 
-    try:
-        #self.serial_lock.acquire()
-        serial_response = ""
-        serial_port_name = sat_tracker_app.config['SERIAL_PORT_NAME']
-        serial_port = serial.Serial(str(serial_port_name), 9600, rtscts=True,dsrdtr=True, timeout=serial_timeout) 
-        sat_tracker_app.logger.info("User: " + str(pwd.getpwuid(os.getuid()).pw_name) + " is about to Send Serial Command: "+str(serial_command)+" to: "+ str(serial_port_name) )
 
-        # Send Command
-        serial_command += "\n"
-        serial_port.write(serial_command.encode())
+    # Send Serial Command, Get Serial Response
+    def execute_serial_command(self, serial_command, serial_timeout=0):
 
-        # If TimeOut Is 0 Then Return Immediately, Otherwise Wait For a Response to Arrive On the Serial Port
-        if(0 == serial_timeout):
-            serial_response = 0
-        else:
-            bytes_carraigereturn = bytes("\r")
-            bytes_linefeed = bytes("\n")  
-            characters_recieved = ""
-            continue_reading=True
-            while continue_reading:
-                byte_next = serial_port.read()
-                char_next = byte_next.decode("utf-8")
+        try:
+            #self.serial_lock.acquire()
+            serial_response = ""
+            serial_port_name = self.name_port_rotator
+            serial_port = serial.Serial(str(serial_port_name), self.speed_serial, rtscts=True,dsrdtr=True, timeout=serial_timeout) 
+            logging.info("User: " + str(pwd.getpwuid(os.getuid()).pw_name) + " is about to Send Serial Command: "+str(serial_command)+" to: "+ str(serial_port_name) )
 
-                # Continue Reading Bytes From the Serial Port Until We Find a NewLine Charater ("\n") LineFeed (LF) 0x0A
-                if byte_next:
-                    if ((byte_next == bytes_linefeed) or (byte_next == bytes_carraigereturn)):
-                        continue_reading=False
-                    else:
-                        characters_recieved += char_next         
-                    char_next = ''
-                    byte_next = 0
-            serial_response = characters_recieved
-            sat_tracker_app.logger.info("User: " + str(pwd.getpwuid(os.getuid()).pw_name) + " Received Serial Response: "+str(serial_response)+" to: "+ str(serial_port_name) )
+            # Send Command
+            serial_command += "\n"
+            serial_port.write(serial_command.encode())
 
-        #Close Port, Return Result
-        serial_port.close()
-        return serial_response
+            # If TimeOut Is 0 Then Return Immediately, Otherwise Wait For a Response to Arrive On the Serial Port
+            if(0 == serial_timeout):
+                serial_response = 0
+            else:
+                bytes_carraigereturn = bytes("\r")
+                bytes_linefeed = bytes("\n")  
+                characters_recieved = ""
+                continue_reading=True
+                while continue_reading:
+                    byte_next = serial_port.read()
+                    char_next = byte_next.decode("utf-8")
 
-    except serial.SerialException as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        sat_tracker_app.logger.error(exc_type, fname, exc_tb.tb_lineno)
-        sat_tracker_app.log_exception(e)
-        print(exc_type, fname, exc_tb.tb_lineno)
-        print(e)
-        return redirect("http://"+socket.gethostname()+"/polarity", code=302)
+                    # Continue Reading Bytes From the Serial Port Until We Find a NewLine Charater ("\n") LineFeed (LF) 0x0A
+                    if byte_next:
+                        if ((byte_next == bytes_linefeed) or (byte_next == bytes_carraigereturn)):
+                            continue_reading=False
+                        else:
+                            characters_recieved += char_next         
+                        char_next = ''
+                        byte_next = 0
+                serial_response = characters_recieved
+                logging.info("User: " + str(pwd.getpwuid(os.getuid()).pw_name) + " Received Serial Response: "+str(serial_response)+" to: "+ str(serial_port_name) )
 
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        sat_tracker_app.logger.error(exc_type, fname, exc_tb.tb_lineno)
-        sat_tracker_app.log_exception(e)
-        print(exc_type, fname, exc_tb.tb_lineno)
-        print(e)
-        return redirect("http://"+socket.gethostname()+"/polarity", code=302)
-    
-    finally:
-        #serial_lock.release()
+            #Close Port, Return Result
+            serial_port.close()
+            return serial_response
+
+        except serial.SerialException as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error(exc_type, fname, exc_tb.tb_lineno)
+            logging.exception(e)
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(e)
+            return e.args[0]
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            logging.error(exc_type, fname, exc_tb.tb_lineno)
+            logging.exception(e)
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(e)
+            return e.args[0]
+
+
+    def handle_exception(self, ex):
+            logging.error("An Exception Has Occurred!")        
+            logging.exception(ex)
+            return(ex)  
--- RadioAntenna/PiCode/SatTrackerPi_Gamma/start_tracker.bash
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/start_tracker.bash
@@ -15,12 +15,12 @@ declare -a arraySocatRotctlOutput
 mapfile -t arraySocatRotctlOutput < "$logfileRotctl"
 
 path_rotctld_out="$(cut -d' ' -f7 <<<"${arraySocatRotctlOutput[0]}")"
-path_tracker_in="$(cut -d' ' -f7 <<<"${arraySocatRotctlOutput[1]}")"
+path_tracker_rotctl_in="$(cut -d' ' -f7 <<<"${arraySocatRotctlOutput[1]}")"
 
 echo $(date -u) " The rotctld servicer will write to: ${path_rotctld_out} "
 (`rotctld -m 202 -s 9600 -r ${path_rotctld_out}`)&
 
-echo $(date -u) " The SatTrackerPi listener will listen to: ${path_tracker_in} for Heading-Azimuth and Inclination-Elevation"
+echo $(date -u) " The SatTrackerPi listener will listen to: ${path_tracker_rotctl_in} for Heading-Azimuth and Inclination-Elevation"
 
 echo $(date -u) " Opening Virtual Com Port to Website"
 (`socat -d -d -lf $logfileWebsite pty,raw,echo=0 pty,raw,echo=0`)&
@@ -31,12 +31,13 @@ declare -a arraySocatWebsiteOutput
 mapfile -t arraySocatWebsiteOutput < "$logfileWebsite"
 
 path_webclient_out="$(cut -d' ' -f7 <<<"${arraySocatWebsiteOutput[0]}")"
-path_tracker_client_in="$(cut -d' ' -f7 <<<"${arraySocatWebsiteOutput[1]}")"
+path_tracker_web_in="$(cut -d' ' -f7 <<<"${arraySocatWebsiteOutput[1]}")"
 
-echo $(date -u) " The WebClient will write to: ${path_webclient_out} and the Tracker will listen to: ${path_tracker_client_in} "
-('python ./SatTrackerPiDaemon/SatTrackerPiDaemon.py -r ${path_tracker_in} -w ${path_tracker_client_in} -l ${verbosityLevel}')&
+echo $(date -u) " The WebClient will write to: ${path_webclient_out} and the Tracker WebInput will listen to: ${path_tracker_web_in} "
+('python ./SatTrackerPiDaemon/SatTrackerPiDaemon.py -r ${path_tracker_rotctl_in} -w ${path_tracker_web_in} -l ${verbosityLevel}')&
 sleep 2
-('python ./SatTrackerPiDaemon/SatTrackerPiWebClient.py -r ${path_webclient_out} -l ${verbosityLevel}')&
+
+('python ./SatTrackerPiDaemon/SatTrackerPiWebClient.py  -l ${verbosityLevel} -r ${path_webclient_out} -s 9600 -w "sat-tracker-pi" -i 2')&
 wait
 
 
@@ -44,5 +45,5 @@ wait
 #sleep 2
 #(`chown www-data ${path_webclient_out}`)&
 #sleep 2
-# python ./SatTrackerPiDaemon/SatTrackerPiDaemon.py -r ${path_tracker_in} -w ${path_tracker_client_in} -l ${verbosityLevel}
+# python ./SatTrackerPiDaemon/SatTrackerPiDaemon.py -r ${path_tracker_rotctl_in} -w ${path_tracker_web_in} -l ${verbosityLevel}
 #wait
