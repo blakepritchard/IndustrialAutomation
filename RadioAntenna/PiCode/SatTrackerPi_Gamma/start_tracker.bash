b3d212e0598fa29164961a3abec76848ab47cbe8
Blake Pritchard <BlakePritchard@yahoo.com>
2019-03-07 19:44:58 -0600

    no such file or directory?

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/start_tracker.bash
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/start_tracker.bash
@@ -39,7 +39,7 @@ sleep 2
 
 echo $(date -u) " The WebClient will write to: ${path_webclient_out} and the Tracker WebInput will listen to: ${path_tracker_web_in} "
 echo $PWD
-('python ./SatTrackerPiDaemon/SatTrackerPiDaemon.py -r ${path_tracker_rotctl_in} -w ${path_tracker_web_in} -l ${verbosityLevel}')&
+('/usr/bin/python ./SatTrackerPiDaemon/SatTrackerPiDaemon.py -r ${path_tracker_rotctl_in} -w ${path_tracker_web_in} -l ${verbosityLevel}')&
 #sleep 2
 
 #('python ./SatTrackerPiDaemon/SatTrackerPiWebClient.py  -l ${verbosityLevel} -r ${path_webclient_out} -s 9600 -w "sat-tracker-pi" -i 2')&
