d7c753c504daa58b033105179a9777e3f3dde20e
Blake Pritchard <BlakePritchard@yahoo.com>
2019-03-13 22:16:45 -0500

    turning logging back down

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebServer/SatTrackerWeb.py
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebServer/SatTrackerWeb.py
@@ -116,9 +116,9 @@ def set_rotator_status():
         dict_json_post = json.loads(str_json_post)     
 
         rotator = Rotator.query.get(dict_json_post["id"])
-        sat_tracker_app.logger.info("Found Database Record for Rotator Name:" + rotator.rotator_name )
-        sat_tracker_app.logger.info("Request Data Object:" + str(dict_json_post))
-        sat_tracker_app.logger.info("Request String Data Type:" + str(type(str_json_post))+", Dictionary Object Data Type:" + str(type(dict_json_post)) )
+        sat_tracker_app.logger.debug("Found Database Record for Rotator Name:" + rotator.rotator_name )
+        sat_tracker_app.logger.debug("Request Data Object:" + str(dict_json_post))
+        sat_tracker_app.logger.debug("Request String Data Type:" + str(type(str_json_post))+", Dictionary Object Data Type:" + str(type(dict_json_post)) )
         
         rotator.azimuth_degrees = dict_json_post["azimuth_degrees"]
         rotator.azimuth_steps = dict_json_post["azimuth_steps"]
