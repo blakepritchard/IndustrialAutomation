59613881bdac28e836ab844c7d7cac9bbffd4ddf
Blake Pritchard <BlakePritchard@yahoo.com>
2019-03-15 20:50:33 -0500

    javascript is not python

--- RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebServer/static/javascript/sat_tracker_web.js
+++ RadioAntenna/PiCode/SatTrackerPi_Gamma/SatTrackerPiWebServer/static/javascript/sat_tracker_web.js
@@ -39,14 +39,15 @@ function polarity_tracking_update(is_tracking)
     else{
         $("#btnSetPolarity").prop("disabled",false);
         $("#polarity_next").prop("disabled",false);
-        command = "SP"        
+        command = "SP"
+        int_polarity_speed = 0        
     }
 
     int_polarity_current = Number($("#polarity_current").text())
     int_polarity_speed = Number($("#polarity_speed").val());
 
     var date = new Date();
-    obj_polarity_command = {rotator_id: 1, issue_time: date.toString(), execution_time:"",  command_code:command, command_value:str(int_polarity_speed) };
+    obj_polarity_command = {rotator_id: 1, issue_time: date.toString(), execution_time:"",  command_code:command, command_value:int_polarity_speed };
     json_polarity_command = JSON.stringify(obj_polarity_next);
     resp_polarity_command = jQuery.ajax ({
         url: "/sat_tracker/api/rotator/command",
@@ -63,7 +64,7 @@ function polarity_tracking_update(is_tracking)
 
 function polarity_set(int_polarity_next){
     int_polarity_next = $("#polarity_next").val()
-    obj_polarity_command = {rotator_id: 1, issue_time: date.toString(), execution_time:"",  command_code:"PP", command_value:str(int_polarity_next) };
+    obj_polarity_command = {rotator_id: 1, issue_time: date.toString(), execution_time:"",  command_code:"PP", command_value:int_polarity_next };
     json_polarity_command = JSON.stringify(obj_polarity_next);
     resp_polarity_next = jQuery.ajax ({
         url: "/sat_tracker/api/rotator/command",
